65b90fe230dc0662fa06a250921931e25c347632
JoseGuzman <chato4010@gmail.com>
2022-09-23 00:37:49 -0500

    Se implemento el hilo reloj sin errores

--- RelojJava/src/main.java
+++ RelojJava/src/main.java
@@ -1,9 +1,15 @@
-
+import java.io.IOException;
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		// TODO Auto-generated method stub
-
+		relojProcess reloj = new relojProcess(5, 55, 55);
+		
+		reloj.start();
+		
+		reloj.join();
+		
+		
 	}
 
 }
